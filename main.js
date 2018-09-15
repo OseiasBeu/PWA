40d8ed42f271b83b53ab62790c8012179bdfa7c1
Oseias Beu <oseiasbeu@outlook.com>
2018-09-15 11:57:54 -0300

    Criar PWA-Base

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+let paginaQuestao = document.querySelector("#pergunta");
+let paginaSabre = document.querySelector("#sabre");
+let btnJedi = document.querySelector(".jedi");
+let btnSith = document.querySelector(".sith");
+let lamina = document.querySelector(".lamina");
+
+
+function escolherSith(){
+    paginaSabre.style.display = 'flex';
+    paginaQuestao.style.display = 'none';
+    lamina.style.background = 'linear-gradient(90deg, #e80808, #e48383, #e80808)';
+    lamina.style.boxShadow = '0 0 50px #e80808';
+}
+
+function escolherJedi(){
+    paginaSabre.style.display = 'flex';
+    paginaQuestao.style.display = 'none';
+    lamina.style.background = 'linear-gradient(90deg, #00ff37, #36fd61, #00ff37)';
+    lamina.style.boxShadow = '0 0 50px #00ff37';
+}
+
+btnSith.onclick = escolherSith;
+btnJedi.onclick = escolherJedi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Registra o service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
